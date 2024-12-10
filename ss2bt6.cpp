98f86a8a578bb15d8451b85861d5231ffbeb7407
catim07 <cuongdfle@gmail.com>
2024-12-10 13:34:55 +0700

    Add files via upload

---
+++ ss2bt6.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#define pi 3.14
+int main(){
+	float chuvi,dientich;
+	float bankinh = 10.3;
+	chuvi = 2 * pi * bankinh;
+	dientich= pi * bankinh * bankinh;
+	printf("do dai ban kinh la: %.2f\n",bankinh);
+	printf("chu vi la: %.2f\n",chuvi);
+	printf("dien tich la: %.2f\n",dientich);
+	
+}
+
